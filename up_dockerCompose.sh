60eed2183dbdeffbeceaac55c4bd9ede019a3c8f
nuanyang26 <1090936383@qq.com>
2024-11-04 17:07:36 +0800

    优化docker-compose-[e/s].yaml 添加各容器的restart参数，env为always，service为unless-stopped。 up_dockerCompose.sh添加-p 参数指定项目名区分 env与service

--- up_dockerCompose.sh
+++ up_dockerCompose.sh
@@ -1,3 +1,3 @@
-docker compose -f docker-compose-env.yml up -d &&
+docker compose -f docker-compose-env.yml -p daijia-env up -d &&
 sleep 20 &&
-docker compose -f docker-compose-service.yml  up -d
+docker compose -f docker-compose-service.yml -p daijia-service up -d
