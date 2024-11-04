fb057000e80741df6c7bdbf3cd26ff14bb6ff224
nuanyang26 <1090936383@qq.com>
2024-11-04 17:23:41 +0800

    单独创建一个docker网络，并使两个项目env/service加入到该网络

--- down_dockerCompose.sh
+++ down_dockerCompose.sh
@@ -1,3 +1,3 @@
-docker compose -f docker-compose-service.yml down &&
+docker compose -f docker-compose-service.yml -p daijia-service down &&
 sleep 10 &&
-docker compose -f docker-compose-env.yml down
+docker compose -f docker-compose-env.yml -p daijia-env down
--- up_dockerCompose.sh
+++ up_dockerCompose.sh
@@ -1,3 +1,4 @@
+docker network create daijia
 docker compose -f docker-compose-env.yml -p daijia-env up -d &&
 sleep 20 &&
 docker compose -f docker-compose-service.yml -p daijia-service up -d
